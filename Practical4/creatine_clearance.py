86025e9f11f9879f0d504cb69b21b54f95b36d22
ZDY <dongyang.25@intl.zju.edu.cn>
2026-03-13 14:41:46 +0800

    Update creatine_clearance.py

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -2,23 +2,23 @@
 # (0<Age<100, 20<Weight<80, 0<Creatine Clearance<100) and then calculates the creatine clearance.
 # If the gender is female, the result is multiplied by 0.85, otherwise it is not. The formula used is:
 # CrCl = ((140 - age) * weight) / (72 * creatine clearance) * 0.85 (if the gender is female)
-a=int(input("Enter age: "))#age
-w=int(input("Enter weight: "))#weight
-c=int(input("Enter creatine clearance: "))#creatine clearance
-g=str(input("Enter gender (Male/Female): "))#gender
-if a>0 and a<100:
-    if w>20 and w<80:
-        if c>0 and c<100:#confirming that all the variables value is within the right interval
-            if g=="Female":
-               Q=140-a
-               W=Q*w
-               E=72*c
+age=int(input("Enter age: "))#age
+weight=int(input("Enter weight: "))#weight
+creatine=int(input("Enter creatine clearance: "))#creatine clearance
+gender=str(input("Enter gender (Male/Female): "))#gender
+if age>0 and age<100:
+    if weight>20 and weight<80:
+        if creatine>0 and creatine<100:#confirming that all the variables value is within the right interval
+            if gender=="Female":
+               Q=140-age
+               W=Q*weight
+               E=72*creatine
                CrCl=W*0.85/E
                print("The creatine clearance is: ",CrCl)
             elif g=="Male":
-                Q=140-a
-                W=Q*w
-                E=72*c
+                Q=140-age
+                W=Q*weight
+                E=72*creatine
                 CrCl=W/E
                 print("The creatine clearance is: ",CrCl)
             else:
